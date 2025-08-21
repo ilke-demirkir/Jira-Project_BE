8c94a73cf7e0fdb44c6720666f5c9b796592ba3d
Yasin Mert Özer <yasinmert215@gmail.com>
2025-08-21 11:37:45 +0300

    project entity ayar

--- src/Domain/Entities/Project.cs
+++ src/Domain/Entities/Project.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
     public class Project
     {
-        Guid Id { get; set; }
-        Guid TenantId { get; set; }
+        public Guid Id { get; set; }
+
+        // Foreign key to Tenant
+        public Guid TenantId { get; set; }
         public Tenant Tenant { get; set; }
-        string Name { get; set; }
-        Guid OwnerId { get; set; }
-        ICollection<User> Member { get; set; }
-        ICollection<TaskItem> Tasks { get; set; }
+
+        public string Name { get; set; }
+
+        // Foreign key to User (Owner)
+        public Guid OwnerId { get; set; }
+        public User Owner { get; set; }
+
+        // Many-to-many: Members via TenantUser
+        public ICollection<TenantUser> Members { get; set; }
+
+        // One-to-many: Tasks
+        public ICollection<TaskItem> Tasks { get; set; }
     }
 }
